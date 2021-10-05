e4a0c6d3fb0be0ced91720ed96315a0762199dae
rthakur1312 <67025716+rthakur1312@users.noreply.github.com>
2021-10-05 14:23:43 -0400

    Update main.js

--- main.js
+++ main.js
@@ -48,7 +48,7 @@ camera.rotation.y = 45/180*Math.PI;
     let loader = new GLTFLoader();
 
     let obj;
-    loader.load("cheddar3d.glb", function(gltf){
+    loader.load("SaltVinegar3d.glb", function(gltf){
         obj = gltf.scene;
         var box = new THREE.Box3().setFromObject( obj );
 	 var center = new THREE.Vector3();
