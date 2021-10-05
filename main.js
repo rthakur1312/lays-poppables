30a1e88fbdda5893a67ff734fb7d28c44af348a0
rthakur1312 <67025716+rthakur1312@users.noreply.github.com>
2021-10-05 14:17:10 -0400

    Update main.js

--- main.js
+++ main.js
@@ -48,7 +48,7 @@ camera.rotation.y = 45/180*Math.PI;
     let loader = new GLTFLoader();
 
     let obj;
-    loader.load("BBQ.gltf", function(gltf){
+    loader.load("cheddar3d.glb", function(gltf){
         obj = gltf.scene;
         var box = new THREE.Box3().setFromObject( obj );
 	 var center = new THREE.Vector3();
@@ -56,7 +56,7 @@ camera.rotation.y = 45/180*Math.PI;
 	 obj.position.sub( center );
         
         scene.add(gltf.scene);
-           // obj.scale.set(0.5,0.5,0.5);
+           // obj.scale.set(18,18,18);
     });
     // loader.load("scene.gltf", function(gltf){
     //     obj = gltf.scene;
